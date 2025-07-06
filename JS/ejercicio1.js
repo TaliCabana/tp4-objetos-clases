c1bcc8997a2a7ad4fa07e5777712ea8d00d42f01
Tali <talicabana@gmail.com>
2025-07-06 13:41:10 -0300

    Resolví el ejercicio 1 del TP de Objetos

--- JS/ejercicio1.js
+++ JS/ejercicio1.js
@@ -7,18 +7,60 @@ objeto.apadar()       El auto se apagó
 
 // Notación literal
 const auto = {
-    // Propiedades
-    color: plateado,
-    marca: Toyota,
-    modelo: Corolla,
-    estado: apagado,
+  // Propiedades
+  color: "plateado",
+  marca: "Toyota",
+  modelo: "Corolla",
+  encendido: false, // primero no está encendido
 
-    // Método para encender:
-    encender: function(){
-        console.log(this) // Objeto
-        if (this.estado){
-            document.writeln(`<p>El auto debe encenderse</p>`)
-        }
-        
+  // Método para encender:
+  encender: function () {
+    console.log(this); // Objeto
+    if (this.encendido === false) {
+      // si el auto está apagado
+      this.encendido = true;
+      document.writeln("El auto se encendió.<br>");
+    } else {
+      // el auto no está apagado, o sea está encendido
+      document.writeln("El auto ya estaba encendido.<br>");
     }
+  },
+
+  // Método para apagar el auto:
+  apagar: function () {
+    if (this.encendido === true) {
+      this.encendido = false;
+      document.writeln("El auto estaba encendido y se apagó.<br>");
+    } else {
+      document.writeln("El auto ya estaba apagado. <br>");
+    }
+  },
+};
+
+const accion = prompt(
+  "¿Queres encender o apagar el auto? Escribí ENCENDER o APAGAR"
+);
+
+if ((encendido = false)) {
+  if (accion === "ENCENDER") {
+    auto.encender();
+  } else if (accion === "APAGAR") {
+    auto.apagar();
+  } else {
+    document.writeln("No elegiste una opción válida");
+  }
+} else {
+  if (accion === "ENCENDER") {
+    auto.encender();
+  } else if (accion === "APAGAR") {
+    auto.apagar();
+  } else {
+    document.writeln("No elegiste una opción válida");
+  }
 }
+
+/*
+auto.encender(); // Tendría que verse "El auto se encendió."
+auto.apagar();   // Tendría que verse "El auto estaba encendido y se apagó."
+auto.apagar();   // Tendría que verse "El auto ya estaba apagado."
+*/
